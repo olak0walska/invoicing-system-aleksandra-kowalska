ed14de3a8f0548bb7a684a0ce610b01441042510
Aleksandra Kowalska <okowalska57@gmail.com>
2023-03-03 19:31:16 +0100

    z lekcji

--- app/src/main/java/pl/futurecollars/invoicing/App.java
+++ app/src/main/java/pl/futurecollars/invoicing/App.java
@@ -1,5 +1,12 @@
 package pl.futurecollars.invoicing;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
@@ -31,5 +38,51 @@ public class App {
     System.out.println(service.getAll());
 
     service.delete(id);
+
+    System.out.println(invoice.toString());
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.registerModule(new JavaTimeModule());
+    objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    objectMapper.enable(SerializationFeature.INDENT_OUTPUT );
+
+    try {
+      String invoiceAsJason = objectMapper.writeValueAsString(List.of(invoice));
+      System.out.println(invoiceAsJason);
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
+    }
+
+    try {
+      objectMapper.writeValue(new File("invoice.json"), invoice);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+
+    try {
+      Invoice invoiceFromFile = objectMapper.readValue(new File("invoice.json"), Invoice.class);
+      System.out.println(invoiceFromFile );
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+    ObjectMapper objectMapperYaml = new ObjectMapper(new YAMLFactory());
+    objectMapperYaml.findAndRegisterModules();
+    objectMapperYaml.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    try {
+      objectMapperYaml.writeValue(new File("invoice.yaml"), invoice);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+    try {
+     Invoice invoiceFromYaml = objectMapperYaml.readValue(new File("invoice.yaml"), Invoice.class);
+      System.out.println(invoiceFromYaml);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+
   }
 }
