c8699961199d66c192d9889f18f5b90542be0ea0
Aleksandra Kowalska <okowalska57@gmail.com>
2023-03-15 18:44:24 +0100

    task3

--- src/main/java/pl/futurecollars/invoicing/config/Config.java
+++ src/main/java/pl/futurecollars/invoicing/config/Config.java
@@ -1,6 +1,7 @@
 package pl.futurecollars.invoicing.config;
 
 public class Config {
+
   public static final String ID_FILE_LOCATION = "db/id.txt";
   public static final String DATABASE_LOCATION = "db/invoices.txt";
 }
