280ef08e8ecf8bf0f781684cc7000abd45db4489
Aleksandra Kowalska <okowalska57@gmail.com>
2023-04-08 19:46:37 +0200

    task9

--- src/test/groovy/pl/futurecollars/invoicing/controller/InvoiceControllerIntegrationTest.groovy
+++ src/test/groovy/pl/futurecollars/invoicing/controller/InvoiceControllerIntegrationTest.groovy
@@ -1,6 +1,7 @@
 package pl.futurecollars.invoicing.controller
 
 import org.springframework.http.MediaType
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import static pl.futurecollars.invoicing.TestHelpers.invoice
