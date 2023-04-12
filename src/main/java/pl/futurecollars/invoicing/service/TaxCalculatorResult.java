2d1fdf0f119f85272a78ad9529c4dc741d05827f
Aleksandra Kowalska <okowalska57@gmail.com>
2023-04-12 18:47:00 +0200

    poprawione checkstyle

--- src/main/java/pl/futurecollars/invoicing/service/TaxCalculatorResult.java
+++ src/main/java/pl/futurecollars/invoicing/service/TaxCalculatorResult.java
@@ -4,7 +4,6 @@ import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.extern.jackson.Jacksonized;
 
 @AllArgsConstructor
