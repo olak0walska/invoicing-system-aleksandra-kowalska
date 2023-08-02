8e2d80a3d16268d7110c0a68cf3b60ece4365eaf
Bartosz Kamyszek <bartoszkamyszek1@gmail.com>
2023-08-02 21:04:12 +0200

    fix

--- src/test/groovy/pl/futurecollars/invoicing/db/jpa/JpaDatabaseIntegrationTest.groovy
+++ src/test/groovy/pl/futurecollars/invoicing/db/jpa/JpaDatabaseIntegrationTest.groovy
@@ -2,12 +2,12 @@ package pl.futurecollars.invoicing.db.jpa
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
-import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.annotation.IfProfileValue
 import pl.futurecollars.invoicing.db.AbstractDatabaseTest
 import pl.futurecollars.invoicing.db.Database
 
-@SpringBootTest
+@DataJpaTest
+@IfProfileValue(name = "spring.profiles.active", value = "jpa")
 class JpaDatabaseIntegrationTest extends AbstractDatabaseTest {
 
     @Autowired
